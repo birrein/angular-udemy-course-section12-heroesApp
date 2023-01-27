e3a8f8362dd3c325137235640b4ef65afdbc5d06
Manuel Marín <manuel.marin.t@gmail.com>
2023-01-26 22:51:01 -0300

    Angular Flex layout added

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { HerousRoutingModule } from './herous-routing.module';
+
 import { AddComponent } from './pages/add/add.component';
 import { SearchComponent } from './pages/search/search.component';
 import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListComponent } from './pages/list/list.component';
-import { HerousRoutingModule } from './herous-routing.module';
 
 @NgModule({
   declarations: [
@@ -15,6 +18,6 @@ import { HerousRoutingModule } from './herous-routing.module';
     HomeComponent,
     ListComponent,
   ],
-  imports: [CommonModule, HerousRoutingModule],
+  imports: [CommonModule, FlexLayoutModule, HerousRoutingModule],
 })
 export class HeroesModule {}
